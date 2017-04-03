76f405b901df5d4ab492f02e60fec9a61f675917
Victor Ingman <viregistermail@gmail.com>
2017-04-03 16:07:19 +0200

    added tool to merge schneider csv logs

---
+++ tools/schneider_merge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'Merging logs from Schneider...'
+cat *Schneider1.csv >> SchneiderLol1.csv
+cat *Schneider2.csv >> SchneiderLol2.csv
+cat *Schneider3.csv >> SchneiderLol3.csv
+cat *Schneider4.csv >> SchneiderLol4.csv
+echo 'Done!'
