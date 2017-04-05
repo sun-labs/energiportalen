5620067eb7add33c6fac7572458c6779cf6a1aa9
Victor Ingman <viregistermail@gmail.com>
2017-04-05 16:41:42 +0200

    SQL: average value certain unit, extract data keys for unit, reset table queries.

--- tools/export_column_to_data_value.sql
+++ tools/export_column_to_data_value.sql
@@ -13,22 +13,28 @@ SELECT * FROM unit_keys WHERE unit_id IN (SELECT id FROM units WHERE name LIKE '
 
 SELECT * FROM unit_data;
 
+-- clear table from data
+DELETE FROM unit_data;
+
+-- reset auto_increment
+ALTER TABLE unit_data AUTO_INCREMENT = 1;
+
 /*
 * Manually add each field for each unit.
 */
 INSERT INTO unit_data
 	(unit_id, unit_key, value, timestamp)
 SELECT
-    (SELECT id FROM units WHERE name = 'Cluster1'),
+    (SELECT id FROM units WHERE name = 'Cluster1'), -- (CHANGE)
     (SELECT id FROM unit_keys WHERE 
-        name = 'F15_AS3_CCtrl_Unit1_P_L2' 
+        name = 'F15_AS3_CCtrl_Unit1_Volt_L1_N' -- (CHANGE)
         AND 
         unit_id = (
-            SELECT id FROM units WHERE name = 'Cluster1'
+            SELECT id FROM units WHERE name = 'Cluster1' -- (CHANGE)
         )
     ),
-    col2,
-    col1
+    col_5, -- value of data key (CHANGE)
+    col_1 -- timestamp
 FROM
     Cluster_1_1;
 
@@ -38,9 +44,12 @@ FROM
 * TODO loop the row instead of looping the column over and over.
 */
 INSERT INTO unit_data
-	(unit_id, unit_key, value, timestamp)
+	(unit_id, 
+    unit_key, 
+    value, 
+    timestamp)
 SELECT
-    (SELECT id FROM units WHERE name = 'Cluster1'),
+    (SELECT id FROM units WHERE name = 'Cluster1'), -- Unit_id
     (SELECT id FROM unit_keys WHERE 
         name IN (
             SELECT name FROM unit_keys WHERE unit_id = (
@@ -48,7 +57,35 @@ SELECT
             )
         )
     ),
-    col_2,
-    col_1
+    col_2, -- value of key
+    col_1 -- timestamp
 FROM
     Cluster_1_1;
+
+/*
+* Räkna ut average på ett visst data för en viss enhet
+*/
+SELECT avg(value) FROM unit_data WHERE 
+	unit_id = (
+		SELECT id FROM units WHERE name = 'Cluster2'
+	) AND 
+	unit_key = (
+		SELECT id FROM unit_keys WHERE name = 'F15_AS3_CCtrl_Unit1_P_L3' AND 
+		unit_id = (
+			SELECT id FROM units WHERE name = 'Cluster2'
+		)
+	);
+	
+/*
+* Plockar ut all data som finns för en viss enhet
+*/
+SELECT DISTINCT ud.unit_key, uk.name 
+FROM unit_data as ud
+INNER JOIN unit_keys as uk
+ON ud.unit_key = uk.id
+WHERE ud.unit_id = (
+	SELECT id FROM units WHERE name = 'Cluster3'
+);
+
+-- Cluster1, Cluster2, Cluster3, Schneider, Vader
+-- Cluster1 => [ 1, 2, 3, 4, 5, 6, 7, 8 ];
