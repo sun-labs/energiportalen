aabdf1b302b0bffd8eb7f875ef63a3f215cafe8b
Linus Frosteryd <linus.frosteryd@gmail.com>
2017-04-13 11:31:11 +0200

    Navbar for desktop and small fix in css for mobile

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,7 +4,15 @@ import '../styles/NavBar.css';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <button>SIGN IN</button>
+      <div className="logo-wrap">
+        <h1>ENERGIPORTALEN.</h1>
+        <h2>sun is shining for lemons</h2>
+      </div>
+      <div className="sign-wrap">
+        <input type="text" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="password" />
+        <button>SIGN IN</button>
+      </div>
     </div>
   );
 }
