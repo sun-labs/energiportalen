443d2e232eaac0d1db6e3125864085de58cb96ba
Victor Ingman <viregistermail@gmail.com>
2017-04-13 14:21:56 +0200

    english information about query

---
+++ backend/server/queries/keys_for_unit_name.sql
@@ -0,0 +1,10 @@
+/*
+* get data keys for a specific unit by name
+*/
+SELECT DISTINCT ud.unit_key, uk.name 
+FROM unit_data as ud
+INNER JOIN unit_keys as uk
+ON ud.unit_key = uk.id
+WHERE ud.unit_id = (
+	SELECT id FROM units WHERE name = ?
+);
