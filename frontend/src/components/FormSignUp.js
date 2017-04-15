515eb3719474546dd5ded801bf71e6380bd9f2d2
Victor Ingman <viregistermail@gmail.com>
2017-04-15 17:46:22 +0200

    general forms for sign in and sign up, adding onChange listeners for each input element supplied.

--- frontend/src/components/FormSignUp.js
+++ frontend/src/components/FormSignUp.js
@@ -9,12 +9,44 @@ class FormSignUp extends Component {
     console.log(API_SIGNUP);
   }
 
+  handleChange(e) {
+    // TODO store in state
+    console.log(e.target.value);
+  }
+
+  /*
+  * Bind each input-child passed into this component to the
+  *  function that handles changes and stores state.
+  * Ex: 
+  *   <input type="text" /> 
+  *   converts to 
+  *   <input type="text" onchange="handleChange" />
+  */
+  renderChildren(children) {
+    return React.Children.map(children, (child) => {
+      // does the current element contain an array of subelements? recursive call.
+      if(child.props && child.props.children && typeof child.props.children === 'object') {
+        return React.cloneElement(child, {
+          children: this.renderChildren(child.props.children)
+        });
+      // is the element of type input? add onChange listener
+      } else if(child.type === 'input') {
+        return React.cloneElement(child, {
+          onChange: this.handleChange
+        });
+      // nothing, just return without modifying
+      } else {
+        return child;
+      }
+    });
+  }
+
   render() {
     const className = this.props.className ? this.props.className : undefined;
     return(
-        <form className={ className } onSubmit={ this.handleSubmit }>
-          { this.props.children }
-        </form>
+      <form className={ className } onSubmit={ this.handleSubmit }>
+        { this.renderChildren(this.props.children) }
+      </form>
     );
   }
 
