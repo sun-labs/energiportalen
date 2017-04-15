02b8396c54762cc760a5f2b313fa9285152e8124
Kasper Ramström <kasper.ram@gmail.com>
2017-04-15 19:56:18 +0200

    removed non-used code (merge-mistake)

--- frontend/src/components/FormSignUp.js
+++ frontend/src/components/FormSignUp.js
@@ -14,33 +14,6 @@ const FormSignUp = ({ className, children }) => {
     console.log(`[SIGN][${e.target.name.toUpperCase()}] ${e.target.value}`);
   }
 
-  /*
-  * Bind each input-child passed into this component to the
-  *  function that handles changes and stores state.
-  * Ex: 
-  *   <input type="text" /> 
-  *   converts to 
-  *   <input type="text" onchange="handleChange" />
-  */
-  const renderChildren = (children) => {
-    return React.Children.map(children, (child) => {
-      // does the current element contain an array of subelements? recursive call.
-      if(child.props && child.props.children && typeof child.props.children === 'object') {
-        return React.cloneElement(child, {
-          children: this.renderChildren(child.props.children)
-        });
-      // is the element of type input? add onChange listener
-      } else if(child.type === 'input') {
-        return React.cloneElement(child, {
-          onChange: handleChange
-        });
-      // nothing, just return without modifying
-      } else {
-        return child;
-      }
-    });
-  }
-
   return (
     <FormGeneric className={ className ? className : '' } onSubmit={ handleSubmit } onChange={ handleChange }>
       { children ? children : '' }
