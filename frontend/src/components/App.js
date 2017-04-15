b6c30e2af14d3fd3374c26b26ce5b6ff39677349
Kasper Ramström <kasper.ram@gmail.com>
2017-04-15 20:21:48 +0200

    proposal: Section1,2,3,4 --> Sections

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,19 +5,22 @@ import '../styles/App.css';
 
 // COMPONENT IMPORTS
 import NavBar from './NavBar';
-import Section1 from './Section1';
-import Section2 from './Section2';
-import Section3 from './Section3';
-import Section4 from './Section4';
+import Sections from './Sections';
+
+// import Section1 from './Section1';
+// import Section2 from './Section2';
+// import Section3 from './Section3';
+// import Section4 from './Section4';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar/>
-      <Section1 />
+      <Sections/>
+      {/*<Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
+      <Section4 />*/}
     </div>
   );
 }
