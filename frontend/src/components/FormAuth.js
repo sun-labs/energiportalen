5b45328085ca191df0c2fe89e68a9ebdab17d084
Victor Ingman <viregistermail@gmail.com>
2017-04-15 18:27:49 +0200

    Generic forms, less duplicate code

--- frontend/src/components/FormAuth.js
+++ frontend/src/components/FormAuth.js
@@ -1,52 +1,24 @@
 import React, { Component } from 'react';
 import { API_AUTH } from '../assets/APIRoutes';
+import FormGeneric from './FormGeneric';
 
 class FormAuth extends Component {
 
   handleSubmit(e) {
-    // TODO send ajax request to backend
     e.preventDefault();
-    console.log(API_AUTH);
+    console.log(`[AUTH] ${API_AUTH}`);
   }
 
   handleChange(e) {
-    // TODO store in state
-    console.log(e.target.value);
+    console.log(`[AUTH][${e.target.name.toUpperCase()}] ${e.target.value}`);
   }
-
-  /*
-  * Bind each input-child passed into this component to the
-  *  function that handles changes and stores state.
-  * Ex: 
-  *   <input type="text" /> 
-  *   converts to 
-  *   <input type="text" onchange="handleChange" />
-  */
-  renderChildren(children) {
-    return React.Children.map(children, (child) => {
-      // does the current element contain an array of subelements? recursive call.
-      if(child.props && child.props.children && typeof child.props.children === 'object') {
-        return React.cloneElement(child, {
-          children: this.renderChildren(child.props.children)
-        });
-      // is the element of type input? add onChange listener
-      } else if(child.type === 'input') {
-        return React.cloneElement(child, {
-          onChange: this.handleChange
-        });
-      // nothing, just return without modifying
-      } else {
-        return child;
-      }
-    });
-  }
-
+  
   render() {
     const className = this.props.className ? this.props.className : undefined;
     return(
-        <form className={ className } onSubmit={ this.handleSubmit }>
-          { this.renderChildren(this.props.children) }
-        </form>
+        <FormGeneric className={ className } onSubmit={ this.handleSubmit } onChange={ this.handleChange }>
+          { this.props.children }
+        </FormGeneric>
     );
   }
 
