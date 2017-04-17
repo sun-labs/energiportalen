45f593265c77186cb13f9774f7721528e92d0e58
Victor Ingman <viregistermail@gmail.com>
2017-04-17 16:10:44 +0200

    Styling for popup and responsive syntax to buttons.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import './styles/Reset.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <App />,
