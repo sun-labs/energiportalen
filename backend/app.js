6ddb3eb2b652be2310817973e849b8e8d7127af4
Victor Ingman <viregistermail@gmail.com>
2017-04-20 16:57:41 +0200

    added routers for api and units, extraction of data works.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import apiRouter from './apiRouter';
 const app = express();
+
+app.use('/1', apiRouter);
 
 app.get('/', (req, res) => {
   res.write('Welcome to the api.');
---
+++ backend/config.js
@@ -0,0 +1,11 @@
+const config = {
+  mysql: {
+    host: '127.0.0.1',
+    user: 'energiportalen',
+    password: '***REMOVED***',
+    database: 'energiportalen',
+    port: 8889
+  }
+};
+
+export default config;
