bb1b57e0932b2f957eee59384476c4d33b89125c
Kasper Ramström <kasper.ram@gmail.com>
2017-04-20 11:36:53 +0200

    hiding x-axis labels

--- frontend/src/Portal/components/LineBlock.js
+++ frontend/src/Portal/components/LineBlock.js
@@ -3,11 +3,21 @@ import { Line } from 'react-chartjs-2';
 
 import '../styles/LineBlock.css';
 
+const options = {
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [{
+      display: false
+    }]
+  },
+}
+
 const data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
-      label: 'My First dataset',
       fill: false,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
@@ -34,7 +44,7 @@ const Block = (props) => {
   return (
     <div className="block line-block">
       {/*You could have some data right here, not there, here, at Sun Labs, and you could have it beautifully visualized*/}
-      <Line className="chart" data={data} />
+      <Line className="line-chart" data={data} options={options} />
     </div>
   );
 }
