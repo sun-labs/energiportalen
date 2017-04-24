6f45412b79658caba99606ed069f248e11b6caa3
Kasper Ramström <kasper.ram@gmail.com>
2017-04-24 11:51:17 +0200

    --> using fake data so we don't have to run database&server when doing frontend work

--- frontend/src/Portal/components/Home.js
+++ frontend/src/Portal/components/Home.js
@@ -25,52 +25,61 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    for (let i = 0; i < 3; i++) {
-      axios.get(`http://localhost:4000/1/units/4/${85+i}`)
-        .then((res) => {
+    /* ADD THE FOLLOWING TO USE REAL DATABASE DATA */
+    // for (let i = 0; i < 3; i++) {
+    //   axios.get(`http://localhost:4000/1/units/4/${85+i}`)
+    //     .then((res) => {
 
-          this.setState({
-            data: [ ...this.state.data, { data: res.data.data.splice(1, res.data.data.length), label: 'what is it'} ],
-            timestamps: [ ...this.state.timestamps, res.data.timestamps.splice(1, res.data.timestamps.length) ],
-            title: [ ...this.state.title, 'data' ],
-            location: [ ...this.state.location, 'Akademiska sjukhuset' ]
-          })
+    //       this.setState({
+    //         data: [ ...this.state.data, { data: res.data.data.splice(1, res.data.data.length), label: 'what is it'} ],
+    //         timestamps: [ ...this.state.timestamps, res.data.timestamps.splice(1, res.data.timestamps.length) ],
+    //         title: [ ...this.state.title, 'data' ],
+    //         location: [ ...this.state.location, 'Akademiska sjukhuset' ]
+    //       })
 
 
-        })
-        .catch((err) => {
-          console.log(err);
-      });
-    }
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //   });
+    // }
   }
 
   render(){
-    const { data, timestamps, title, location } = this.state;
+    /* ADD THE FOLLOWING TO USE REAL DATABASE DATA */
 
-    let props = [];
+    // const { data, timestamps, title, location } = this.state;
 
-    for (let i = 0; i < timestamps.length; i++ ) {
-      props = [ 
-        ...props, 
-        { 
-          data: [data[i]],
-          labels: timestamps[i],
-          title: title[i],
-          location: location[i]
-        }
-      ]
-    }
+    // let props = [];
+
+    // for (let i = 0; i < timestamps.length; i++ ) {
+    //   props = [ 
+    //     ...props, 
+    //     { 
+    //       data: [data[i]],
+    //       labels: timestamps[i],
+    //       title: title[i],
+    //       location: location[i]
+    //     }
+    //   ]
+    // }
 
     return (
       <div className="content">
         <Link to={`${ROOT}/addblock`} className="add-block block">+ ADD BLOCK</Link>
 
-        { props.map((item, index) => {
+        {/* ADD THE FOLLOWING TO USE REAL DATABASE DATA */}
+        {/*{ props.map((item, index) => {
             return ( 
               <LineBlock key={index} {...item} />
             );
           }) 
-        }
+        }*/}
+
+        <LineBlock/>
+        <LineBlock/>
+        <LineBlock/>
+        <LineBlock/>        
       </div>
     );
   }
