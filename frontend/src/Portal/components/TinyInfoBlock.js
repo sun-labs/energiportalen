ae5af73e4b49121d108f5923258a2e188446a606
Kasper Ramström <kasper.ram@gmail.com>
2017-04-24 16:00:53 +0200

    tiny-info-block stuff

--- frontend/src/Portal/components/TinyInfoBlock.js
+++ frontend/src/Portal/components/TinyInfoBlock.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 import '../styles/TinyInfoBlock.css';
 
-const TinyInfoBlock = () => {
+const ph_TimeSpan = '365d';
+const ph_Key = 'PRODUCERAD ENERGI';
+const ph_Value = '500';
+const ph_Unit = 'MWh';
+
+const TinyInfoBlock = ({ timeSpan = ph_TimeSpan, key = ph_Key, value = ph_Value, unit = ph_Unit }) => {
   return (
     <div className="tiny-info-block">
-      Tiny Info Block
+      <div className="header">
+        <span className="timespan">{timeSpan}</span>
+        <span className="key">{key}</span>
+      </div>
+      
+      <div className="value-unit">
+        <span className="value">{value}</span>
+        <span className="unit">{unit}</span>
+      </div>
     </div>
   );
 }
