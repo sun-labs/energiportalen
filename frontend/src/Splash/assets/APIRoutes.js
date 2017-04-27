2daf57e566061a4db617c24eaba1d3774d82caae
Kasper Ramström <kasper.ram@gmail.com>
2017-04-27 13:08:03 +0200

    sign up wohoo & stores token

--- frontend/src/Splash/assets/APIRoutes.js
+++ frontend/src/Splash/assets/APIRoutes.js
@@ -1,12 +1,14 @@
-const HOST = 'localhost:3000';
-const VERSION = 1;
+const HOST = 'localhost:4000';
+// const VERSION = 1;
 const SECURE = false;
 const PROTOCOL = SECURE ? 'https' : 'http';
 
 const URL = `${PROTOCOL}://${HOST}`;
 export const URL_TERMS = `${URL}/terms`;
 
-const API_URL = `${URL}/${VERSION}`;
+// NOTE såg inte anledningen till VERSION?
+// const API_URL = `${URL}/${VERSION}`; 
+const API_URL = `${URL}`;
 export const API_AUTH = `${API_URL}/auth`;
 export const API_SIGNUP = `${API_URL}/signup`;
 export const API_FORGOT_PASSWORD = `${API_URL}/forgot`;
