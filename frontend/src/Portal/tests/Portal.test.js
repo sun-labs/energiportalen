770501b32a2891fc89d67806a01d6c44f4615acc
Kasper Ramström <kasper.ram@gmail.com>
2017-05-03 20:54:39 +0200

    added component render tests

---
+++ frontend/src/Portal/tests/Portal.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AddBlock from '../components/AddBlock';
+import Block from '../components/Block';
+import BotNavBar from '../components/BotNavBar';
+import Home from '../components/Home';
+import LineBlock from '../components/LineBlock';
+import Portal from '../components/Portal';
+import TableBlock from '../components/TableBlock';
+import TinyInfoBlock from '../components/TinyInfoBlock';
+import TopNavBar from '../components/TopNavBar';
+
+global.CanvasRenderingContext2D = () => {}; // TODO find better solution for this 
+
+const withRouter = (component) => {
+  const div = document.createElement('div');  
+  ReactDOM.render(<Router>{component}</Router>, div)
+}
+
+it('AddBlock renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddBlock/>, div);
+});
+
+it('Block renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Block/>, div);
+});
+it('BotNavBar renders without crashing', () => {
+  withRouter(<BotNavBar/>);
+});
+
+it('Home renders without crashing', () => {
+  withRouter(<Home/>);
+});
+it('LineBlock renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<LineBlock/>, div);
+});
+
+it('Portal renders without crashing', () => {
+  withRouter(<Portal/>);
+});
+it('TableBlock renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TableBlock/>, div);
+});
+
+it('TinyInfoBlock renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TinyInfoBlock/>, div);
+});
+it('TopNavBar renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TopNavBar/>, div);
+});
---
+++ frontend/src/Splash/tests/Splash.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import FormAuth from '../components/FormAuth';
+import FormSignUp from '../components/FormSignUp';
+import MessageBox from '../components/MessageBox';
+import NavBar from '../components/NavBar';
+import Popup from '../components/Popup';
+import Section from '../components/Section';
+import Sections from '../components/Sections';
+import Splash from '../components/Splash';
+import TextContent from '../components/TextContent';
+
+const withRouter = (component) => {
+  const div = document.createElement('div');  
+  ReactDOM.render(<Router>{component}</Router>, div)
+}
+
+it('FormAuth renders without crashing', () => {
+  withRouter(<FormAuth/>);
+});
+
+it('FormSignUp renders without crashing', () => {
+  withRouter(<FormSignUp/>);
+});
+it('MessageBox renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MessageBox/>, div);
+});
+
+it('NavBar renders without crashing', () => {
+  withRouter(<NavBar/>);
+});
+it('Popup renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Popup/>, div);
+});
+
+it('Section renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Section/>, div);
+});
+it('Sections renders without crashing', () => {
+  withRouter(<Sections/>);
+});
+
+it('Splash renders without crashing', () => {
+  withRouter(<Splash/>);
+});
+it('TextContent renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TextContent/>, div);
+});
