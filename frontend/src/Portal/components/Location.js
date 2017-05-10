85483e248297066793b3d422cd3797eb5198fd33
Linus Frosteryd <linus.frosteryd@gmail.com>
2017-05-10 18:09:40 +0200

    added fetch from location to compare view

--- frontend/src/Portal/components/Location.js
+++ frontend/src/Portal/components/Location.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../Splash/assets/APIRoutes';
 
+import { Link } from 'react-router-dom';
+import { ROOT } from './Portal';
+
 // STYLE IMPORTS
 import '../styles/Home.css';
+import '../styles/Locations.css';
 
 // COMPONENT IMPORTS
 import FacBlock from './FacBlock';
 
 
-class DashboardLocations extends Component {
+class Location extends Component {
 
   constructor () {
     super()
@@ -20,7 +24,7 @@ class DashboardLocations extends Component {
 
 componentWillMount(){
   const token = localStorage.getItem('token');
-
+  
   axios.get(API_URL+'/locations', { headers: {Authorization: token}}).then(Response => {
     var location = [];
     var responsData = Response.data.length;
@@ -31,7 +35,7 @@ componentWillMount(){
       let Desc = Response.data[i].description;
       let Image = Response.data[i].image;
 
-      let DashLocation = {
+      let LocLocation = {
         Id: Id, 
         Name: Name,
         City: City,
@@ -39,7 +43,7 @@ componentWillMount(){
         Image: Image
       }
 
-      location.push(DashLocation);
+      location.push(LocLocation);
     }
     this.setState({
       locations: location
@@ -54,11 +58,11 @@ componentWillMount(){
   let lengthLocations = this.state.locations.length;
   for (var i = 0; i < lengthLocations; i++){
     let location = this.state.locations[i];
-    content.push(<FacBlock fac={location.Image} title={location.Name} key={location.Id} />)
+    content.push(<Link to={`${ROOT}/locations/` +  location.Id} key={location.Id}><FacBlock fac={location.Image} title={location.Name} subtitle={location.Desc} key={location.Id} /></Link>)
   }
 
     return (
-        <div className="facblock-wrapper">
+        <div className="FacBlock-wrap">
           { content }
         </div>
     );
@@ -66,4 +70,4 @@ componentWillMount(){
 
 }
 
-export default DashboardLocations;
+export default Location;
