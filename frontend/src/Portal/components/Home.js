7b653aad3b32581d87ffceaa812e8f21cd61f79a
Linus Frosteryd <linus.frosteryd@gmail.com>
2017-05-10 13:52:05 +0200

    add location in home

--- frontend/src/Portal/components/Home.js
+++ frontend/src/Portal/components/Home.js
@@ -10,6 +10,7 @@ import '../styles/Home.css';
 import LineBlock from './LineBlock';
 import TableBlock from './TableBlock';
 import IlluBlock from './IlluBlock';
+import FacBlock from './FacBlock';
 
 class Home extends Component {
   constructor() {
@@ -30,6 +31,16 @@ class Home extends Component {
   render() {
     return (
       <div className="content">
+        <Link to={`${ROOT}/addlocation`} className="blockk add-block">+ ADD LOCATION</Link>
+        <div className="text-block">
+          <h1>FAVORITE LOCATIONS<span className="inline-button"> add location + </span></h1>
+          <h2> you may save or remove your own personally defined locations for easier access. </h2>
+        </div>
+        <div className="facblock-wrapper">
+          <FacBlock/>
+          <FacBlock/>
+          <FacBlock/>
+        </div>
         <Link to={`${ROOT}/addblock`} className="blockk add-block">+ ADD BLOCK</Link>
         <div className="text-block">
             <h1>blocks <span className="inline-button"> add block + </span></h1>
