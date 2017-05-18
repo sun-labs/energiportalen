9d8c53a4894d559b09758fa478be862288e3f024
Kasper Ramström <kasper.ram@gmail.com>
2017-05-18 17:23:21 +0200

    added react-select for add-block

--- frontend/src/Portal/components/AddBlock.js
+++ frontend/src/Portal/components/AddBlock.js
@@ -1,25 +1,116 @@
 import React, { Component } from 'react';
 import '../styles/AddBlock.css';
 
+import Select from 'react-select';
+import '../styles/react-select.css';
 import Block from './Block';
 
 class IlluBlock extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      location: '',
+      type: '',
+      timespan: '',
+      content: ''
+    }
+  }
+
   render() {
     const {
       value = 534.3,
     } = this.props;
+
+    const {
+      location,
+      type,
+      timespan,
+      content
+    } = this.state;
 
     const blockInfo = {
       value,
       type: 'ADD',
       title: 'ADD BLOCK'
     }
+
+    const locationOptions = [
+      { value: 'Akademiska Sjukhuset', label: 'Akademiska Sjukhuset' },
+      { value: 'Origo', label: 'Origo' }
+    ];
+
+    const typeOptions = [
+      { value: 'Line Chart', label: 'Line Chart' },
+      { value: 'Table Chart', label: 'Table Chart' },
+      { value: 'Smartphone charges', label: 'Smartphone charges' },
+      { value: 'Scooter thingy', label: 'Scooter thingy' }
+    ];
+
+    const timespanOptions = [
+      { value: '1h', label: '1h' },
+      { value: '24h', label: '24h' },
+      { value: '7d', label: '7d' },
+      { value: '365d', label: '365d' }
+    ];
+
+    const contentOptions = [
+      { value: 'kWh', label: 'kWh' },
+      { value: 'temperature', label: 'temperature' },
+      { value: 'humidity', label: 'humidity' },
+      { value: 'radiation', label: 'radiation' }
+    ];
+
     return(
     <Block className="blockk-add" {...blockInfo}>
-      <input className="chooseLoc-add" placeholder="CHOOSE LOCATION"/>
-      <input className="chooseBlock-add" placeholder="CHOOSE BLOCK TYPE"/>
-      <input className="chooseTime-add" placeholder="TIME SPAN"/>
-      <input className="chooseContent-add" placeholder="CHOOSE CONTENT"/>
+      <Select
+        name="form-field-name"
+        value={location}
+        options={locationOptions}
+        placeholder="CHOOSE LOCATION"
+        clearable={true}
+        className="chooseLoc-add"
+        onChange={(e) =>
+          { e 
+            ? this.setState({ location: e.value }) 
+            : this.setState({ location: '' })}
+        }/>
+      <Select
+        name="form-field-name"
+        value={type}
+        options={typeOptions}
+        placeholder="CHOOSE BLOCK TYPE"
+        clearable={true}
+        className="chooseBlock-add"
+        onChange={(e) =>
+          { e 
+            ? this.setState({ type: e.value }) 
+            : this.setState({ type: '' })}
+        }/>
+      <Select
+        name="form-field-name"
+        value={timespan}
+        options={timespanOptions}
+        placeholder="TIME SPAN"
+        clearable={true}
+        className="chooseTime-add"
+        onChange={(e) =>
+          { e 
+            ? this.setState({ timespan: e.value }) 
+            : this.setState({ timespan: '' })}
+        }/>
+      <Select
+        name="form-field-name"
+        value={content}
+        options={contentOptions}
+        placeholder="CHOOSE CONTENT"
+        clearable={true}
+        className="chooseContent-add"
+        onChange={(e) =>
+          { e 
+            ? this.setState({ content: e.value }) 
+            : this.setState({ content: '' })}
+        }/>
       <button>
         SAVE BLOCK
       </button>
