dfc5ed07cccb7014be81ff31456cfa2a1500d868
Victor Ingman <viregistermail@gmail.com>
2017-12-09 15:33:53 +0100

    Split run code and api code for energiportalen tool

--- tools/sunlabs/api_energiportalen.py
+++ tools/sunlabs/api_energiportalen.py
@@ -19,11 +19,12 @@ def _query(query, params = None):
       cur.execute(query)
     else:
       cur.execute(query, params)
+    con.commit()
     return cur
-  except (mysql.Error, mysql.Warning) as e:
-    print(e)
+  except (mysql.Error, mysql.Warning) as err:
+    print("[RAW] {}".format(err))
     return None
-
+  
 def _entity_exists(table, id = None, name = None):
   query = "SELECT * FROM {}".format(table)
   if id is not None:
@@ -106,52 +107,3 @@ def add_unit_data(unit_id = None, unit_key = None, value = None, timestamp = Non
   """
   cur = _query(query, [unit_id, unit_key, value, timestamp])
   return cur.lastrowid
-  
-
-# def add_unit(unit_name = None, unit_location = None)
-
-location = {
-  'name': 'test',
-  'image': None,
-  'description': None,
-  'country': 'SWE',
-  'city': 'Uppsala',
-}
-unit = { 'name': 'SALAHEBY1' }
-new_loc = add_location(**location)
-new_unit = add_unit(**unit)
-bind_loc_unit = bind_unit_location(new_unit, new_loc)
-new_uk = add_unit_key(**{ 
-    'name': 'TESTKEY',
-    'unit_id': new_unit,
-    'notes': 'testing',
-    'si_unit_id': 14,
-})
-
-
-ts = time.time()
-timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-new_ud = add_unit_data(**{
-  'unit_id': new_unit,
-  'unit_key': new_uk,
-  'value': 10.00102012,
-  'timestamp': timestamp,
-})
-
-con.commit()
-
-lid_exists = location_exists(id = new_loc)
-lname_exists = location_exists(name = location['name'])
-
-print lid_exists
-print lname_exists
-
-# print new_uk
-# print new_ud
-# print(new_loc, new_unit, bind_loc_unit)
-
-# print get_units()
-# print get_unit_keys(5)
-# print get_unit_keys()
-# print unit_exists(6)
-# print unit_exists(-53)
