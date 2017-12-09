93d975658c2fc0a58735eba62f01fba748df609a
Victor Ingman <viregistermail@gmail.com>
2017-12-09 14:17:14 +0100

    Add locations to database

--- tools/sunlabs/api_energiportalen.py
+++ tools/sunlabs/api_energiportalen.py
@@ -5,7 +5,7 @@ con = None
 cur = None
 try:
   con =  mysql.connect(**C.database)
-  cur = c.cursor()
+  cur = con.cursor()
 except mysql.OperationalError as err:
   print("Can't connect to database.")
   print("[RAW] {}".format(err))
@@ -31,6 +31,30 @@ def unit_exists(unit_id):
   query = "SELECT * FROM units WHERE id = %s"
   cur.execute(query, [unit_id])
   return cur.fetchone() is not None
+
+def add_location(name = None, image = None, description = None, country = None, city = None):
+  query = """
+    INSERT INTO locations
+      (name, image, description, country, city)
+    VALUES
+      (%s, %s, %s, %s, %s)
+  """
+  cur.execute(query, [name, image, description, country, city])
+  return cur.lastrowid
+
+
+# def add_unit(unit_name = None, unit_location = None)
+
+location = {
+  'name': 'test',
+  'image': None,
+  'description': None,
+  'country': 'SWE',
+  'city': 'Uppsala',
+}
+new_loc = add_location(**location)
+print new_loc
+con.commit()
 
 # print get_units()
 # print get_unit_keys(5)
