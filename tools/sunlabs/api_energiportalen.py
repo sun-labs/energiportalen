b78ac3bbf7806f5212931cd2e94f694b91807cf2
Victor Ingman <viregistermail@gmail.com>
2017-12-09 12:09:43 +0100

    Added sunlabs api

---
+++ tools/sunlabs/api_energiportalen.py
@@ -0,0 +1,37 @@
+import MySQLdb as mysql
+import CONFIG as C
+
+cur = None
+try:
+  c =  mysql.connect(**C.database)
+  cur = c.cursor()
+except mysql.OperationalError as err:
+  print("Can't connect to database.")
+  print("[RAW] {}".format(err))
+
+# get every unit 
+def get_units():
+  query = "SELECT * FROM units"
+  cur.execute(query)
+  return cur.fetchall()
+
+# get every unit_key or filter by unit_id
+def get_unit_keys(unit_id = None):
+  query = "SELECT * FROM unit_keys as uk"
+  if unit_id is not None:
+    query += " WHERE unit_id = %s"
+    cur.execute(query, [unit_id])
+  else:
+    cur.execute(query)
+  return cur.fetchall()
+
+def unit_exists(unit_id):
+  query = "SELECT * FROM units WHERE id = %s"
+  cur.execute(query, [unit_id])
+  return cur.fetchone() is not None
+
+# print get_units()
+# print get_unit_keys(5)
+# print get_unit_keys()
+# print unit_exists(6)
+# print unit_exists(-53)
