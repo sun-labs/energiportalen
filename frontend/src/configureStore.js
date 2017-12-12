772686d9e4599b3bd5514008fcd67280c82eb83f
Victor Ingman <viregistermail@gmail.com>
2017-12-12 12:08:33 +0100

    Added support for redux devtools

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
@@ -10,10 +10,10 @@ const configureStore = () => {
 		middlewares.push(createLogger());
 	}
 
-	return createStore(
-    portalApp,
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // redux devtools
+	return createStore(portalApp, composeEnhancers( //composeEnhancers => redux devtools
 		applyMiddleware(...middlewares)
-	);
+	));
 };
 
 export default configureStore;
