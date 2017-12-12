56ff547f0e4bc83df92afb9706c7617a676e893d
kramstrom <kasper.ram@gmail.com>
2017-12-12 17:19:53 +0100

    fixed victor

--- frontend/src/Portal/components/DashboardLocations.js
+++ frontend/src/Portal/components/DashboardLocations.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import addimg from '../../imgs/add-button.png';
 import FacBlock from './FacBlock';
 import PropTypes from 'prop-types';
 import * as routeConstants from '../../constants/routeConstants';
