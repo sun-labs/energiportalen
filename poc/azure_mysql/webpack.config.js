808674953d7c68c497562fc60dd30f87a58779bb
Victor Ingman <viregistermail@gmail.com>
2017-12-13 16:52:06 +0100

    Added proof of concept for azure, works

---
+++ poc/azure_mysql/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: [
+      './app.js'
+  ],
+  output: {
+     filename: 'build/index.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+     extensions: ['.js', '.jsx'] // common extensions
+  },
+  target: 'node',
+};
