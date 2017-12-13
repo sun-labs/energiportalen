b6da3943849b586e76fcb875af5ba426e5eb1516
Victor Ingman <viregistermail@gmail.com>
2017-12-13 18:00:36 +0100

    Fixed cache, clear cache, implement new config file for api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { API_URL } from './Splash/assets/APIRoutes';
+import config from './config';
 
-const CACHE_LIFE = 1;//24 * 60 * 60 // 24 hours
+const CACHE_LIFE = config.cache.duration; //24 * 60 * 60 // 24 hours
+const CACHE_PREFIX = config.cache.prefix;
 const token = getToken()
+
+cleanCache() // every reload
 
 let api = axios.create({
   baseURL: API_URL,
@@ -12,34 +16,71 @@ let api = axios.create({
   }
 })
 
+function cacheKey(key) {
+  return `${CACHE_PREFIX}-${key}`;
+}
 
-// function store(key, val) {
-//   if(localStorage) {
-//     const obj = { stored: Date.now(), val }
-//     localStorage.setItem(key, JSON.stringify(obj))
-//     return true;
-//   }
-//   return false;
-// }
-
-function get(key) {
+// store value in cache
+function store(key, val) {
+  key = cacheKey(key);
+  const obj = { stored: Date.now(), val }
+  const cachedObj = JSON.stringify(obj);
   if(localStorage) {
-    const val = JSON.parse(localStorage.getItem(key))
-    if(val) {
-      const age = (Date.now() - val.stored) / 1000;
-      if(age < CACHE_LIFE) {
-        return val.val
-      }
-    }
+    localStorage.setItem(key, cachedObj)
+    return true;
+  }
+  return false;
+}
+
+// get cached object value
+function get(key) {
+  key = cacheKey(key);
+  const obj = _get(key);
+  if(obj && !_isOld(obj)) {
+    return obj.val
   }
   return undefined;
+}
+
+// get raw cache object
+function _get(key) {
+  let data = undefined;
+  if(localStorage) {
+    data = localStorage.getItem(key);
+  }
+  return JSON.parse(data);
+}
+
+// remove items 
+function _remove(key) {
+  if (localStorage) {
+    localStorage.removeItem(key);
+    return true;
+  }
+  return false;
+}
+
+// check if cached obj is old => true
+function _isOld(obj) {
+  const age = (Date.now() - obj.stored) / 1000;
+  return age < CACHE_LIFE;
+}
+
+function cleanCache() {
+  const cached = Object.keys(localStorage).filter((key) => (key.indexOf(CACHE_PREFIX) > -1));
+  cached.forEach((item) => {
+    const obj = _get(item);
+    if(_isOld(obj)) {
+      _remove(item);
+    }
+  })
 }
 
 function call(method, url, data, cb) {
   method = method.toUpperCase()
   const key = `${method}-${url}-${data}`;
   const cached = get(key);
-  if(/*cached*/false) {
+  if(cached) {
     cb(cached)
   } else {
     api.request({
@@ -47,8 +88,8 @@ function call(method, url, data, cb) {
       url,
       data,
     })
-    .then((res) => { // TODO do not store complete request object, store data.
-      // store(key, res);
+    .then((res) => { // TODO: do not store complete request object, store data.
+      store(key, res);
       cb(res);
     });
   }
